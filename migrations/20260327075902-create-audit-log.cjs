2d1b792e874afe67a8cbd5a0b2d57dda1df1255f
meislovepotato <fmikejordan1213@gmail.com>
2026-03-27 18:06:44 +0800

    migrations

---
+++ migrations/20260327075902-create-audit-log.cjs
@@ -0,0 +1,34 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('AuditLogs', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      action: {
+        type: Sequelize.STRING
+      },
+      ipAddress: {
+        type: Sequelize.STRING
+      },
+      userId: {
+        type: Sequelize.UUID
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('AuditLogs');
+  }
+};
