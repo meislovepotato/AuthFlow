3e99658ccbcd0b388a83627104a3eacf076f5af5
meislovepotato <fmikejordan1213@gmail.com>
2026-05-25 19:38:56 +0800

    database

--- config/config.cjs
+++ config/config.cjs
@@ -2,10 +2,7 @@ require("dotenv").config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: "mysql",
+    DATABASE_URL: process.env.DATABASE_URL,
+    dialect: "postgres",
   },
 };
