166cda8a9d2a40bb198f9b76f8cce54e9e3493c1
meislovepotato <fmikejordan1213@gmail.com>
2026-09-05 23:44:00 +0800

    added 20260905120000-ensure-default-roles.cjs, which safely creates missing ADMIN and USER roles

---
+++ migrations/20260905130000-align-user-role-id-type.cjs
@@ -0,0 +1,17 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.changeColumn('Users', 'roleId', {
+      type: Sequelize.INTEGER,
+      allowNull: true,
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.changeColumn('Users', 'roleId', {
+      type: Sequelize.UUID,
+      allowNull: true,
+    });
+  },
+};
